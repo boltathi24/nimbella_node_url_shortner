a02f0027c8bd96823934cd83829a7be3d86b4f77
boltathi <athithya24@gmail.com>
2020-12-03 17:36:10 +0530

    url shortner in Node.js

--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -8,12 +8,13 @@ class Shortner extends Component {
   }
       state = {
         actual_url: null,
-        shorten_url_response: null
+        shorten_url_response: null,
       };
       constructor(props) {
         super(props);
         this.state = {actual_url: null,
-          shorten_url_response: null};
+          shorten_url_response: null,
+        linkvalid:null};
       }
 
      
@@ -21,16 +22,35 @@ class Shortner extends Component {
       handleClick = () => {
         //do something
         this.setState({shorten_url_response: null});
-        this.setData()
+        if(this.ValidURL())
+        {
+          this.setData()
+        }
+        else
+        {
+          this.setState({linkvalid: "Please enter valid URL"});
+        }
+       
       }
 
+       ValidURL() {
+        // var pattern = new RegExp(/^(ftp|http|https):\/\/[^ "]+$/)
+        var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+        '(\\#[-a-z\\d_]*)?$','i'); // fragment locator        
+      return !!pattern.test(this.state.actual_url);
+    }
       setUrl= (e)=> {
+        this.setState({linkvalid: null});
         this.setState({actual_url: e.target.value});
         console.log(this.state)
     }
 
        setData() {
-         
+         console.log()
     axios.post("https://"+window.location.hostname+"/api/s/url", {actual_url:this.state.actual_url},{ headers: {'content-type': 'application/json'}})
         .then(response => this.setState({ shorten_url_response: response.data.short_url }));
     }
@@ -43,11 +63,19 @@ class Shortner extends Component {
       header = <label className="labelclass">Your shortened URL is</label>
 
     } 
+    let error
+    if (this.state.linkvalid) {
+
+      error = <label className="labelerror">Please enter valid url</label>
+
+    } 
     return (
       <div>
-        <textarea  type="text"  className="textboxHeight"onChange={ this.setUrl } placeholder="paste the link to shorten.."/><br></br>
-       <input type="button" value="submit" onClick={this.handleClick}></input><br></br><br></br>
+        <textarea class="textboxHeight" type="url" required pattern="^(http|https)://\.(.*)" className="textboxHeight"onChange={ this.setUrl } placeholder="paste the link to shorten.."/><br></br> <br></br>
+       <input type="button" className="button" value="submit" onClick={this.handleClick}></input><br></br><br></br>
+       {error}<br></br>
        {header}<br></br><br></br>
+       
        <a className="resultTextArea" href={ this.state.shorten_url_response} target="_blank" rel="noopener noreferrer">{ this.state.shorten_url_response}</a>
       </div>
     );
